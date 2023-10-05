b3dab23cfd036c657b4af65b057a567d9b9235ea
Knight of belief <slbdenis.victor@gmail.com>
2023-10-05 21:42:30 +0300

    Refactor

--- storage.go
+++ storage.go
@@ -77,7 +77,7 @@ func (s *PostgresStore) CreateAccount(acc *Account) error {
 	return nil
 }
 func (s *PostgresStore) UpdateAccount(account *Account) error {
-	query := `UPDATE accounts SET first_name = $1, last_name = $2, balance = $3 WHERE id = $4`
+	query := `UPDATE account SET first_name = $1, last_name = $2, balance = $3 WHERE id = $4`
 
 	_, err := s.db.Exec(query, account.FirstName, account.LastName, account.Balance, account.ID)
 	if err != nil {
--- types_test.go
+++ types_test.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewAccount(t *testing.T) {
-	acc, err := NewAccount("a", "b", "hunter")
+	_, err := NewAccount("a", "b", "hunter")
 	assert.Nil(t, err)
 
-	fmt.Printf("%+v\n", acc)
+	// fmt.Printf("%+v\n", acc)
 }
